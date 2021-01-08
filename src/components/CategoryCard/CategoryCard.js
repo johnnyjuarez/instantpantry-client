5aa18168ed6f9f3f12aeb70582f1a167c0876cf2
johnnyjuarez <johnnyjuarez1992@gmail.com>
2021-01-08 15:11:06 -0600

    added categorycards component

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -14,13 +14,17 @@ export default function CategoryCard(props) {
     history.push(`/category/${props.id}/addItem`)
   }
 
+  const deleteHandler = () => {
+    alert('deleted');
+  }
+
   return (
     <div className='categoryCard'>
       <h3>{props.title}</h3>
       <div className='btn-box'>
         <button onClick={viewItemsHandler}>View Items</button>
         <button onClick={addItemHandler}>Add Item</button>
-        <button>Delete</button>
+        <button onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   )
