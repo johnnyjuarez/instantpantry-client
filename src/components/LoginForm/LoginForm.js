854c556ccab95dbbca37198d96e02a4397dacc91
johnnyjuarez <johnnyjuarez1992@gmail.com>
2021-01-13 22:06:35 -0600

    connected backend for login component and dashboard component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useHistory} from 'react-router-dom'
+import TokenService from '../../services/token-service';
+
+import config from '../../config';
 
 export default function LoginForm(props) {
   let history = useHistory();
 
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
   const loginHandler = (e) => {
+    const credentials = {
+      username,
+      password
+    }
     e.preventDefault();
-    history.push('/dashboard')
+    fetch(`${config.API_ENDPOINT}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+    .then(res => {
+      if(!res.ok) {
+        return res.json().then(e => {
+          return Promise.reject(e);
+        })
+      }
+      return res.json();
+    })
+    .then((data) => {
+      localStorage.setItem('userId', data.id);
+      TokenService.saveAuthToken(data.authToken);
+      history.push('/dashboard');
+    })
+  }
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
   }
 
   return (
@@ -14,9 +53,9 @@ export default function LoginForm(props) {
       <form onSubmit={loginHandler} className='login-form'>
         <h2>Login</h2>
         <label>Username:</label>
-        <input type='text' />
+        <input onChange={onChangeUsername} type='text' />
         <label>Password:</label>
-        <input type='password' />
+        <input onChange={onChangePassword} type='password' />
         <input type='submit' />
       </form>
     </div>
--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -1,28 +1,35 @@
 import React, {useState, useEffect} from 'react'
 import CategoryCard from '../../components/CategoryCard/CategoryCard'
 
+import TokenService from '../../services/token-service'
+
+import config from '../../config';
+
 export default function Dashboard() {
   // state for holding category data
   const [categories, setCategories] = useState([])
 
+  const user_id = localStorage.getItem('userId')
+
   useEffect(() => {
-    // fetch request to setCategories
-    setCategories([
-      {
-        id: 1,
-        title: 'Sample Title',
-        items: [1, 2, 3, 4, 5]
-      },
-      {
-        id: 2,
-        title: 'Sample Title Two',
-        items: [1, 2, 3, 4, 5]
-      },
-    ])
+    console.log('user id', user_id);
+    fetch(`${config.API_ENDPOINT}/category/${user_id}`, {
+      headers: {
+        'content-Type': 'application/json',
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      }
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      setCategories(data);
+    })
   }, [])
 
   const categoryCards = categories.map((category) => {
-    return <CategoryCard key={category.id} id={category.id} title={category.title} />
+    return <CategoryCard key={category.id} id={category.id} title={category.category_title} />
   })
 
   return (
